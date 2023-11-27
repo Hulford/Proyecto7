8c8ea5363e4bec7a7f370032ceb685bcba85f68d
Hulford <moraleshulford@gmail.com>
2023-11-27 02:55:58 -0500

    Servicio e interfaces

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,5 +8,42 @@ import { Component } from '@angular/core';
   ]
 })
 export class SelectorPageComponent {
-
+  public myform: FormGroup = this.fb.group({
+     region: ['', Validators.required],
+     country: ['', Validators.required],
+     borders: ['', Validators.required],
+  })
+  constructor(
+        private fb: FormBuilder,
+       ){}
 }
+
+
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { CountriesRoutingModule } from './countries-routing.module';
+// import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+// import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+
+// @NgModule({
+//   declarations: [
+//     SelectorPageComponent
+//   ],
+//   imports: [
+//     CommonModule,
+//     CountriesRoutingModule,
+//     ReactiveFormsModule,
+//   ]
+// })
+// export class CountriesModule {
+//   public myform: FormGroup = this.fb.group({
+// region: ['', Validators.required],
+// country: ['', Validators.required],
+// borders: ['', Validators.required],
+//   })
+//   constructor(
+//     private fb: FormBuilder,
+//   ){}
+// }
